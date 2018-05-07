2f7386ca2deecf33dee299fd9a49a65a00edae54
Joan' Araujo <wisdombthegame@gmail.com>
2018-05-07 17:33:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,6 @@
-class Dog
+class Dog 
 end
-
-fido = Dog.new
-snoopy = Dog.new
-lassie = Dog.new
-
-
+ 
+ fido = Dog.new
+ snoopy = Dog.new
+ lassie = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
 end
 
-adele_goldberg = Person.new
+adele_goldberg = Person.new 
 alan_kay = Person.new
